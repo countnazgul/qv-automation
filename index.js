c9c82186fdf9036ccce608df7868760fa1d6b0e0
Stefan Stoichev <stefan.stoichev@gmail.com>
2016-11-30 07:55:28 +0000

    v0.0.8
    * module utilization in tasks files
    * fix - clean tasks now works

--- index.js
+++ index.js
@@ -1,48 +1,36 @@
 #!/usr/bin/env node
 
 var gulp = require('gulp');
+var argv = require('yargs').argv;
+
+var tasks = argv._;
 
 //var qvAutomate = function () {
 
-    //function init() {
-        _createTasks();
-        run();
-    //}
+init();
+
+function init() {
+    _createTasks();
+    run();
+}
 
 
-    function run() {
-        //console.log(gulp)
-        //process.emit('senseGo_onRunStart');
-        //try {
-            gulp.series(['test:b'], function (done) {
-                //process.emit('senseGo_onRunFinished');
-                //process.emit('senseGo_onEnd');
-                done();
-            })();
-        }
-        // catch (ex) {
-        //     if (ex) {
-        //         //gUtil.log(gUtil.colors.red(ex.name), ex.message);
-        //         console.log(ex.message)
-        //     }
-        // }
-    //}
-
-    function _createTasks() {
-
-        //var TaskCreator = require('./taskCreator');
-        //var taskCreator = new TaskCreator(gulp, plugins, config);
-
-        //var normalizedPath = path.join(__dirname);
-
-        //fs.readdirSync(normalizedPath).forEach(function (file) {
-        require("./lib/qv-automate")(gulp);
-        //done();
-        
-        
-
-        //});
+function run() {
+    try {
+        gulp.series(tasks, function (done) {
+            done();
+        })();
     }
+    catch (ex) {
+        if (ex) {
+            console.log(ex.message)
+        }
+    }
+}
+
+function _createTasks() {
+    require("./lib/qv-automate")(gulp);
+}
 
     // return {
     //     //init: init,
