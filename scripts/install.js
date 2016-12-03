b7e95c563877702879e58b6202500aff9f893b0b
Stefan Stoichev <stefan.stoichev@gmail.com>
2016-12-03 18:07:50 +0000

    v0.0.12
    * post install and uninstall scripts
    * deploy tasks are working
    * main build tasks

--- scripts/install.js
+++ scripts/install.js
@@ -1,1 +1,12 @@
-// to-do
+var mkdirp = require('mkdirp');
+var fs = require('fs');
+var path = require('path');
+var msg = require('gulp-messenger');
+
+var user = process.env['USERPROFILE'].split(path.sep)[2];
+var mainTemp = 'c:\\Users\\' + user + '\\\Documents\\qv-automation\\temp';
+
+mkdirp.sync(mainTemp);
+fs.createReadStream(__dirname + '\\settings_example.json').pipe(fs.createWriteStream('c:\\Users\\' + user + '\\\Documents\\qv-automation\\settings.json'));
+
+msg.Success('Temp folder and settings file were created at c:\\USsers\\' + user + '\\\Documents\\qv-automation' );
--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -1,1 +1,10 @@
-// to-do
+var del = require('del');
+var msg = require('gulp-messenger');
+var path = require('path');
+
+var user = process.env['USERPROFILE'].split(path.sep)[2];
+var mainTemp = 'c:\\Users\\' + user + '\\\Documents\\qv-automation';
+
+del.sync(mainTemp, { force: true });
+
+msg.Success('Temp folder was removed')
